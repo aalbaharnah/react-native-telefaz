a84117d62e63e87bff9bfdb4b1f4cd29237b82b2
aalbaharnah <baharnah.13@gmail.com>
2025-06-28 23:45:45 +0300

    fix: update tmdbApiKey to use EXPO_PUBLIC_TMDB_API_KEY and improve comments in API functions

--- src/lib/api/configs.ts
+++ src/lib/api/configs.ts
@@ -1,4 +1,4 @@
 export default {
     api: `https://api.themoviedb.org/3/`,
-    tmdbApiKey: process.env.TMDB_API_KEY ?? '0fd92481ddb5910a66ea82005307008d',
+    tmdbApiKey: process.env.EXPO_PUBLIC_TMDB_API_KEY,
 }
--- src/lib/api/gets.ts
+++ src/lib/api/gets.ts
@@ -5,17 +5,19 @@ import configs from './configs';
 import { Show } from '../types';
 
 
-
+// Fetch popular movies, TV shows, and documentaries from The Movie Database (TMDB) API
 export async function getMovies() {
     return GET<{ results: Show[] }>(`https://api.themoviedb.org/3/movie/popular?api_key=${configs.tmdbApiKey}&page=1`)
         .then(res => res.results);
 }
 
+// Fetch popular TV shows from The Movie Database (TMDB) API
 export async function getTVShows() {
     return GET<{ results: Show[] }>(`https://api.themoviedb.org/3/tv/popular?api_key=${configs.tmdbApiKey}&page=1`)
         .then(res => res.results);
 }
 
+// Fetch documentary shows from The Movie Database (TMDB) API
 export async function getDocumentaryShows() {
     return GET<{ results: Show[] }>(`https://api.themoviedb.org/3/discover/tv?api_key=${configs.tmdbApiKey}&with_genres=99&page=1`)
         .then(res => res.results);
